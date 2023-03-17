74e0980075c62bb417c839735bba93edd3a239c7
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2023-03-17 13:37:05 +0700

    create card component

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,22 +1,25 @@
 import React from "react";
+import classes from "./AddUser.module.css";
+import Card from "../UI/Card";
 
 const AddUsers = (props) => {
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
 
-    const submitHandler = (event) => {
-        event.preventDefault();
-    };
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={submitHandler}>
+        <label>Username</label>
+        <input type="text" />
 
-    return (
-        <form onSubmit={submitHandler()}>
-            <label>Username</label>
-            <input type="text"/>
+        <label>age</label>
+        <input type="number" />
 
-            <label>age</label>
-            <input type="number"/>
-
-            <button type="submit">Submit</button>
-        </form>
-    )
-}
+        <button type="submit">Submit</button>
+      </form>
+    </Card>
+  );
+};
 
 export default AddUsers;
