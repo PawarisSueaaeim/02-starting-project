b40bf2b6d2d1c0f4ddd9dd60f0bac8f9b8267341
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2023-03-23 11:41:50 +0700

    added state on input

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState }from "react";
 import classes from "./AddUser.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 
 const AddUsers = (props) => {
+  const [useredChange, setUseredChange] = useState('');
+  const [ageChange, setAgeChange] = useState();
+
   const submitHandler = (event) => {
     event.preventDefault();
+    console.log(useredChange, ageChange);
+  };
+
+  const inputuseredOnChangehandler = (event) => {
+    setUseredChange(event.target.value);
+  };
+
+  const inputedAgeOnChangehandler = (event) => {
+    setAgeChange(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={submitHandler}>
         <label>Username</label>
-        <input type="text" />
+        <input type="text" onChange={inputuseredOnChangehandler}/>
 
         <label>age</label>
-        <input type="number" />
+        <input type="number" onChange={inputedAgeOnChangehandler}/>
 
         <Button type="submit">Submit</Button>
       </form>
